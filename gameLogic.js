d395b087fffb3049848ea0ad3f933bdf6fb4a3d3
JTaylor27 <taylorf27tetete@gmail.com>
2015-07-14 11:57:54 -0400

    First Game

---
+++ gameLogic.js
@@ -0,0 +1,52 @@
+var myGame = {
+  config : {
+      lives: 5,
+      speed : 0.4,
+      actionButton: 'leftmouse',
+      endTrigger : 'Trigger.End',
+      screenImages : [
+        {
+          src:'/images/start.png',
+          id : 'mainScreen'
+        },
+        {
+          src : '/images/lose.png',
+          id : 'loseScreen'
+        },
+        {
+          src : '/images/win.png',
+          id : 'winScreen'
+        }
+      ],
+      livesImage : {
+        src : '/images/heart.png',
+        dX:0,
+        dY:10,
+        dWidth:30,
+        dHeight:30
+      },
+      cameraSettings : {
+        offset : {
+          x : 5,
+          y : 0,
+          z : -20
+        },
+        rotation : {
+          x: 0,
+          y: 0,
+          z: 0
+        }
+      }
+  },
+
+  scripts : function(){
+    //TODO this is where you can add your own game logic
+  }
+};
+
+function start(){
+    myGame.game = new A3D.Game.Scroller(myGame.config);
+    myGame.game.loadScene('/Scenes/','my_game.babylon',myGame.scripts);
+}
+
+document.addEventListener( "DOMContentLoaded", start, false );
